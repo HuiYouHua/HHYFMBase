d84d6974f51996609418fa32e7e514debb548445
华惠友 <793316968@qq.com>
2020-04-06 15:55:20 +0800

    no message

---
+++ Example/Pods/Target Support Files/HHYFMBase/HHYFMBase-umbrella.h
@@ -0,0 +1,40 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "Base.h"
+#import "Sington.h"
+#import "XMGConst.h"
+#import "CALayer+PauseAimate.h"
+#import "UIImage+XMGImage.h"
+#import "UIView+XMGLayout.h"
+#import "UIImageView+XMGExtension.h"
+#import "XMGSessionManager.h"
+#import "XMGAlertTool.h"
+#import "XMGCacheTool.h"
+#import "XMGDeviceMessage.h"
+#import "XMGNoticeLocal.h"
+#import "Base.h"
+#import "Sington.h"
+#import "XMGConst.h"
+#import "CALayer+PauseAimate.h"
+#import "UIImage+XMGImage.h"
+#import "UIView+XMGLayout.h"
+#import "UIImageView+XMGExtension.h"
+#import "XMGSessionManager.h"
+#import "XMGAlertTool.h"
+#import "XMGCacheTool.h"
+#import "XMGDeviceMessage.h"
+#import "XMGNoticeLocal.h"
+
+FOUNDATION_EXPORT double HHYFMBaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char HHYFMBaseVersionString[];
+
---
+++ Example/Pods/Target Support Files/Pods-HHYFMBase_Example/Pods-HHYFMBase_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HHYFMBase_Example : NSObject
+@end
+@implementation PodsDummy_Pods_HHYFMBase_Example
+@end
